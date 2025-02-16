815be242d24a5ba8c4224257d0c3dfddbee9290e
Nulo <hi@nulo.in>
2025-02-16 16:55:44 -0300

    disclaimer

--- sitio/src/routes/+layout.svelte
+++ sitio/src/routes/+layout.svelte
@@ -6,9 +6,18 @@
 
 {@render children()}
 
-<footer class="mt-8 flex items-center justify-center gap-3 py-4 text-center text-gray-500">
-	Un experimento de
-	<a href="https://nulo.lol" target="_blank">
-		<img src={NuloInc} alt="Nulo Science Inc." class="h-12" />
-	</a>
+<footer
+	class="mt-8 flex flex-col items-center justify-center gap-3 px-2 py-4 text-center text-gray-500"
+>
+	<p>
+		Los resultados mostrados son generados automaticamente. Siempre verifica la información en la
+		fuente original.
+	</p>
+	<p>Marcas registradas pertenecen a sus respectivos dueños.</p>
+	<div>
+		Un experimento de
+		<a href="https://nulo.lol" target="_blank">
+			<img src={NuloInc} alt="Nulo Science Inc." class="h-12" />
+		</a>
+	</div>
 </footer>
