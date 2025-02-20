f03d8f08fce91828af09a12df59d795149dd3822
Nulo <hi@nulo.in>
2025-02-20 17:01:19 -0300

    scraper: set user agent

--- scraper/lib.ts
+++ scraper/lib.ts
@@ -27,6 +27,9 @@ export async function createBrowserSession() {
   });
 
   const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+  );
 
   await page.goto("https://api.ipify.org?format=json");
   const ip = await page.evaluate(() => {
