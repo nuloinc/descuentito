7597bca590a82c5d4095cc8b307eb9663a8213ee
Nulo <hi@nulo.in>
2025-02-22 15:27:43 -0300

    scraper(coto): perdirle no cambiar cosas

--- scraper/trigger/coto.ts
+++ scraper/trigger/coto.ts
@@ -1,196 +1,106 @@
 import { logger, schedules } from "@trigger.dev/sdk/v3";
 import { google } from "@ai-sdk/google";
 import { z } from "zod";
-import { generateObject, NoObjectGeneratedError, streamObject } from "ai";
-import { CotoPromotion } from "promos-db/schema";
-import { fetchPageData } from "./lib/fetch-page";
+import { NoObjectGeneratedError, streamObject } from "ai";
+import {
+  BasicPromotionSchema,
+  CotoPromotion,
+  LIMITS_PROMPT,
+  PAYMENT_METHODS_PROMPT,
+  RESTRICTIONS_PROMPT,
+} from "promos-db/schema";
 import { savePromotions } from "../lib/git";
+import { createPlaywrightSession } from "../lib";
 
-const promotionSchema = z.object({
-  title: z.string(),
-  category: z.string().optional(),
-  discount: z.object({
-    type: z.enum(["porcentaje", "cuotas sin intereses"]),
-    value: z
-      .number()
-      .describe("0 to 100 for percentage, 0 to 12 for cuotas"),
-  }),
-  validFrom: z.string().describe("YYYY-MM-DD"),
-  validUntil: z.string().describe("YYYY-MM-DD"),
-  weekdays: z
-    .array(
-      z.enum([
-        "Lunes",
-        "Martes",
-        "Miercoles",
-        "Jueves",
-        "Viernes",
-        "Sabado",
-        "Domingo",
-      ])
-    )
-    .optional(),
-  restrictions: z.array(z.string()),
+const promotionSchema = BasicPromotionSchema.extend({
   where: z.array(z.enum(["Coto", "Online"])),
-  paymentMethods: z
-    .array(
-      z.array(
-        z.enum([
-          "Banco Patagonia",
-          "Banco BBVA",
-          "Banco Nación",
-          "Banco Ciudad",
-          "Banco Galicia",
-          "Banco Galicia - Eminent",
-          "Banco Macro",
-          "Banco Santander",
-          "Banco ICBC",
-          "Banco ICBC – Cliente Payroll",
-          "Banco Credicoop",
-          "Banco Credicoop - Plan Sueldo",
-          "Banco Santander",
-          "Banco Comafi",
-          "Banco Galicia Más",
-          "Banco Supervielle",
-          "Banco Columbia",
-          "Banco del Sol",
-          ".Reba",
-          "Mercado Pago",
-          "Dinero en cuenta",
-          "MODO",
-          "Ualá",
-          "Uilo",
-          "NaranjaX",
-          "Cuenta DNI",
-          "Tarjeta Carrefour Prepaga",
-          "Tarjeta Carrefour Crédito",
-          "Tarjeta de crédito VISA",
-          "Tarjeta de débito VISA",
-          "Tarjeta de crédito Mastercard",
-          "Tarjeta de débito Mastercard",
-          "Tarjeta American Express",
-          "Tarjeta Prepaga Mastercard",
-          "Tarjeta de Crédito Coto TCI",
-        ])
-      )
-    )
-    .optional(),
-  membership: z
-    .array(z.enum(["Club La Nacion", "Comunidad Coto"]))
-    .optional(),
-  limits: z.object({
-    maxDiscount: z.number().optional(),
-    explicitlyHasNoLimit: z.boolean().optional(),
-  }),
+  membership: z.array(z.enum(["Club La Nacion", "Comunidad Coto"])).optional(),
 });
 
-const SYSTEM_PROMPT = `You are a helpful assistant that extracts promotions from a text and converts them into structured JSON data with relevant information for argentinian users.
+const SYSTEM_PROMPT = `You are a helpful assistant that extracts promotions from a text and converts them into structured JSON data with relevant information for argentinian users. If the promotion is already in the previous array, copy the previous promotion unless there's a meaningful change.
 
-PAYMENT METHODS
+${PAYMENT_METHODS_PROMPT}
 
-Represent different combinations of payment methods as separate arrays of strings.
-
-If there are multiple combinations possible, represent each and every one of them individually.
-
-Example: Banco Galicia with either VISA or Mastercard credit cards: [["Banco Galicia", "Tarjeta de crédito VISA"], ["Banco Galicia", "Tarjeta de crédito Mastercard"]], NOT merging them like this: [["Banco Galicia", "Tarjeta de crédito VISA", "Tarjeta de crédito Mastercard"]]
-
-"Banco Galicia Más" is a different payment method from "Banco Galicia".
-
-RESTRICTIONS
-
-Do not include irrelevant restrictions that are obvious, such as restrictions related to foreign credit cards, purchase cards, payments in foreign currencies, or social aid programs, or restrictions that specify "Solo para consumo familiar.".
-
-Do not include redundant information that is mentioned elsewhere in the object, such as validity dates, days of the week, payment methods, where the promotion is valid or limits.
-
-Order by relevance, starting with the most relevant restrictions.
+${RESTRICTIONS_PROMPT}
 
 WHERE
 
 "Coto" means the promotion is valid in physical stores, "Online" means it's valid in cotodigital.com.ar.
 
-LIMITS
-
-\`maxDiscount\` is the maximum discount amount in pesos that can be applied to the promotion.
-
-\`explicitlyHasNoLimit\` is true if the promotion explicitly states that there is no limit ("sin tope").`;
+${LIMITS_PROMPT}
+`;
 
 export const cotoTask = schedules.task({
   id: "coto-extractor",
   cron: "0 0 * * *",
-  maxDuration: 300,
+  maxDuration: 1800,
   retry: {
     maxAttempts: 1,
   },
   run: async (payload, { ctx }) => {
-    const { domDescription,page } = await fetchPageData(
-      "coto",
-      "https://www.cotodigital.com.ar/sitios/cdigi/terminos-descuentos",
-      {
-        selector: ".atg_store_company_content",
-        waitForSelector: ".atg_store_company_content",
-      }
-    );
+    let content: string;
+    const source = "coto";
+    {
+      const url =
+        "https://www.cotodigital.com.ar/sitios/cdigi/terminos-descuentos";
 
-    logger.info("DOM Description", { domDescription });
+      await using session = await createPlaywrightSession();
+      const { page } = session;
+      await page.goto(url, { waitUntil: "networkidle" });
 
-    let promotions: CotoPromotion[] = [];
-    const paragraphs = (await Promise.all(
-      (await page.$$('.atg_store_company_content p')).map(async (p) => {
-        const text = await p.evaluate(el => el.textContent);
-        return text?.trim() || "";
-      })
-    )).filter((p: string) => p.length > 0);
+      content =
+        (await page.$eval(
+          ".atg_store_company_content",
+          (el) => el.textContent
+        )) || "";
+      logger.info("Content", { content });
 
-    logger.info("Processing paragraphs", { count: paragraphs.length });
-
-    for (const paragraph of paragraphs) {
-      try {
-        const { object } = await generateObject({
-          model: google("gemini-2.0-flash"),
-          schema: promotionSchema,
-          messages: [
-            {
-              role: "user",
-              content: [
-                {
-                  type: "text",
-                  text: "Extract the promotions from the following text: " + paragraph,
-                },
-              ],
-            },
-          ],
-          system: SYSTEM_PROMPT,
-        });
-
-        logger.info("Extracted promotion from paragraph", { 
-          paragraph,
-          object 
-        });
-          
-        promotions.push({
-          ...object,
-          url: "https://www.cotodigital.com.ar/sitios/cdigi/terminos-descuentos",
-          source: "coto",
-        });
-      } catch (error) {
-        if (NoObjectGeneratedError.isInstance(error)) {
-          logger.warn("No promotion found in paragraph", {
-            paragraph,
-            error: error.message,
-            text: error.text,
-            cause: error.cause,
-          });
-          // Continue processing other paragraphs
-          continue;
-        }
-        // For other errors, log but continue processing
-        logger.error("Error processing paragraph", {
-          paragraph,
-          error,
-        });
+      if (!content) {
+        throw new Error("No content found");
       }
     }
 
-    await savePromotions("coto", promotions);
+    const oldPromotions = await fetch(
+      `https://raw.githubusercontent.com/nuloinc/descuentito-data/refs/heads/main/${source}.json`
+    )
+      .then((res) => res.json())
+      .catch(() => []);
+
+    let promotions: CotoPromotion[] = [];
+    const { elementStream } = streamObject({
+      model: google("gemini-2.0-pro-exp-02-05"),
+      output: "array",
+      schema: promotionSchema,
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: `Here's the previous promotion array: ${JSON.stringify(
+                oldPromotions
+              )}.\n\n Extract the promotions from the following text: ${content}`,
+            },
+          ],
+        },
+      ],
+      system: SYSTEM_PROMPT,
+    });
+
+    try {
+      for await (const object of elementStream) {
+        logger.info("Extracted promotion", { object });
+
+        promotions.push({
+          ...object,
+          url: "https://www.cotodigital.com.ar/sitios/cdigi/terminos-descuentos",
+          source,
+        });
+      }
+    } catch (error) {
+      logger.error("Error processing content", { error });
+    }
+
+    await savePromotions(source, promotions);
   },
 });
