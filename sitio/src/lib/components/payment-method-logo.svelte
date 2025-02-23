910da5d8d047e7a16fd0822c42a24a7cc30cdcd4
Nulo <hi@nulo.in>
2025-02-23 17:12:00 -0300

    mejorar layout

---
+++ sitio/src/lib/components/payment-method-logo.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { logos } from '@/logos';
+	import type { PAYMENT_METHODS } from 'promos-db/schema';
+	import { Badge } from './ui/badge';
+
+	function isPaymentMethod(method: string): method is keyof typeof logos {
+		return method in logos;
+	}
+
+	export let method: (typeof PAYMENT_METHODS)[number];
+</script>
+
+{#if isPaymentMethod(method)}
+	<enhanced:img src={logos[method]} alt={method} class="h-8 w-auto" />
+{:else}
+	{method}
+{/if}
