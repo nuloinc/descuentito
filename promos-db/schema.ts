9ad5bcb749173ff74364aba9b21533bb3c58629f
Nulo <hi@nulo.in>
2025-02-23 18:25:19 -0300

    mejorar prompt, agregar otros medios de pago

--- promos-db/schema.ts
+++ promos-db/schema.ts
@@ -62,6 +62,7 @@ export interface DiaDiscount extends GenericDiscount {
 export interface JumboDiscount extends GenericDiscount {
   source: "jumbo";
   where: ("Jumbo" | "Online")[];
+  paymentMethods?: PaymentMethod[][];
 }
 
 export type Discount =
@@ -91,6 +92,8 @@ export const BANKS_OR_WALLETS = [
   "Banco Supervielle",
   "Banco Columbia",
   "Banco del Sol",
+  "Banco Entre Ríos",
+  "Banco Santa Fe",
   ".Reba",
   "Uilo",
   "NaranjaX",
@@ -99,6 +102,7 @@ export const BANKS_OR_WALLETS = [
   "Tarjeta Carrefour Crédito",
   "Tarjeta de crédito Coto TCI",
   "Tarjeta de crédito Cencosud Mastercard",
+  "Sidecreer",
 ] as const;
 
 export const PAYMENT_METHODS = [
@@ -111,6 +115,8 @@ export const PAYMENT_METHODS = [
   "Tarjeta American Express",
   "Tarjeta Prepaga Mastercard",
 ] as const;
+
+export type PaymentMethod = (typeof PAYMENT_METHODS)[number];
 
 export const BasicDiscountSchema = z.object({
   discount: z.object({
@@ -144,7 +150,7 @@ export const BasicDiscountSchema = z.object({
   }),
 });
 
-export const PAYMENT_METHODS_PROMPT = `PAYMENT METHODS
+export const PAYMENT_METHODS_PROMPT = `## PAYMENT METHODS
 
 Represent different combinations of payment methods as separate arrays of strings.
 
@@ -156,7 +162,7 @@ Inside each combination, sort by bank/wallet, then by payment method (if applica
 
 "Banco Galicia Más" is a different payment method from "Banco Galicia".`;
 
-export const RESTRICTIONS_PROMPT = `RESTRICTIONS
+export const RESTRICTIONS_PROMPT = `## RESTRICTIONS
 
 Do NOT include irrelevant restrictions that are obvious, such as restrictions related to foreign credit cards, purchase cards, payments in foreign currencies, or social aid programs. Do NOT include restrictions that specify "Solo para consumo familiar.", Ley provincial N° 5547 (Mendoza), etc. Do NOT include restrictions that specify "Descuentos, precios y promociones para consumo familiar".
 
@@ -164,7 +170,7 @@ Do not include redundant information that is mentioned elsewhere in the object, 
 
 Order by relevance, starting with the most relevant restrictions.`;
 
-export const LIMITS_PROMPT = `LIMITS
+export const LIMITS_PROMPT = `## LIMITS
 
 \`maxDiscount\` is the maximum discount amount in pesos that can be applied to the discount.
 
