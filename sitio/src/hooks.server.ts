92f51a193d4703cbcda5d3816dd528c55ce8442c
Nulo <hi@nulo.in>
2025-03-07 21:40:04 -0300

    intentar arreglar sentry cloudflare

--- sitio/src/hooks.server.ts
+++ sitio/src/hooks.server.ts
@@ -1,18 +1,21 @@
-// import { sequence } from "@sveltejs/kit/hooks";
-// import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
-// import * as Sentry from '@sentry/sveltekit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
 
-// Sentry.init({
-//   dsn: 'https://fbba219837609bcc6c804579312cbcb6@o4507188153548800.ingest.de.sentry.io/4508938894377040',
+// https://github.com/Lms24/sveltekit-cloudflare-test/blob/main/src/hooks.server.ts
 
-//   tracesSampleRate: 1.0,
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(
+	Sentry.initCloudflareSentryHandle({
+		dsn: 'https://fbba219837609bcc6c804579312cbcb6@o4507188153548800.ingest.de.sentry.io/4508938894377040',
 
-//   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-//   // spotlight: import.meta.env.DEV,
-// });
+		tracesSampleRate: 1.0
 
-// // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-// export const handle = sequence(sentryHandle());
+		// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+		// spotlight: import.meta.env.DEV,
+	}),
+	sentryHandle()
+);
 
-// // If you have a custom error handler, pass it to `handleErrorWithSentry`
-// export const handleError = handleErrorWithSentry();
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
