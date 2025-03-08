9ffcad85486eda5ada8a58ce19513af9f5c75a33
Nulo <hi@nulo.in>
2025-03-07 21:03:19 -0300

    que los cloudflare workers no lloren

--- sitio/src/hooks.server.ts
+++ sitio/src/hooks.server.ts
@@ -1,18 +1,18 @@
-import { sequence } from "@sveltejs/kit/hooks";
-import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
-import * as Sentry from '@sentry/sveltekit';
+// import { sequence } from "@sveltejs/kit/hooks";
+// import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+// import * as Sentry from '@sentry/sveltekit';
 
-Sentry.init({
-  dsn: 'https://fbba219837609bcc6c804579312cbcb6@o4507188153548800.ingest.de.sentry.io/4508938894377040',
+// Sentry.init({
+//   dsn: 'https://fbba219837609bcc6c804579312cbcb6@o4507188153548800.ingest.de.sentry.io/4508938894377040',
 
-  tracesSampleRate: 1.0,
+//   tracesSampleRate: 1.0,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
-});
+//   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+//   // spotlight: import.meta.env.DEV,
+// });
 
-// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
+// // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+// export const handle = sequence(sentryHandle());
 
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+// // If you have a custom error handler, pass it to `handleErrorWithSentry`
+// export const handleError = handleErrorWithSentry();
