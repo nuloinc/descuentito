a8bc2be44cb92a804a65de02327effde154085ab
Nulo <hi@nulo.in>
2025-04-13 10:05:27 -0300

    desactivar replay posthog en defv

--- sitio-react/src/client.tsx
+++ sitio-react/src/client.tsx
@@ -1,18 +1,20 @@
 /// <reference types="vinxi/types/client" />
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/react-start'
-import { createRouter } from './router'
-import { PostHogProvider } from 'posthog-js/react'
+import { hydrateRoot } from "react-dom/client";
+import { StartClient } from "@tanstack/react-start";
+import { createRouter } from "./router";
+import { PostHogProvider } from "posthog-js/react";
 
-const router = createRouter()
+const router = createRouter();
 
-hydrateRoot(document, (
+hydrateRoot(
+  document,
   <PostHogProvider
     apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
     options={{
-      api_host: 'https://us.i.posthog.com',
+      api_host: "https://us.i.posthog.com",
+      disable_session_recording: import.meta.env.DEV,
     }}
   >
     <StartClient router={router} />
   </PostHogProvider>
-))
+);
