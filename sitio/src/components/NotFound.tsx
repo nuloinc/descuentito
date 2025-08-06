a3bdf990ee6a655cda1a5cdcb97312d9bfb9027a
Nulo <hola@nulo.lol>
2025-08-06 15:48:23 -0300

    404 castellano

--- sitio/src/components/NotFound.tsx
+++ sitio/src/components/NotFound.tsx
@@ -4,20 +4,21 @@ export function NotFound({ children }: { children?: any }) {
   return (
     <div className="space-y-2 p-2">
       <div className="text-gray-600 dark:text-gray-400">
-        {children || <p>The page you are looking for does not exist.</p>}
+        {children || <p>La página que estás buscando no existe.</p>}
       </div>
       <p className="flex items-center gap-2 flex-wrap">
         <button
           onClick={() => window.history.back()}
           className="bg-emerald-500 text-white px-2 py-1 rounded uppercase font-black text-sm"
         >
-          Go back
+          Volver
         </button>
         <Link
           to="/"
+          search={{ supermarket: undefined }}
           className="bg-cyan-600 text-white px-2 py-1 rounded uppercase font-black text-sm"
         >
-          Start Over
+          Volver a la página de inicio
         </Link>
       </p>
     </div>
