e3f2ae96b8f9b3e9e716fa7f6d9209c83f252903
Nulo <hola@nulo.lol>
2025-08-18 21:03:10 -0300

    log

--- scraper/trigger/lib/logger.ts
+++ scraper/trigger/lib/logger.ts
@@ -2,7 +2,7 @@ import pino from "pino";
 
 // Create a logger instance
 export const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
+  level: process.env.LOG_LEVEL || "debug",
   transport: {
     target: "pino-pretty",
     options: {
